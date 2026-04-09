1fae2360abdbfe8b3e272eeb11c6333e5971e3a3
adarsh-inamdar <eradarshainamdar@gmail.com>
2026-04-09 09:53:43 +0530

    Fix Express 5 wildcard route

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ if (!fs.existsSync(uploadsDir)){
 }
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) =>
+    app.get('(.*)', (req, res) =>
         res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'))
     );
 } else {
